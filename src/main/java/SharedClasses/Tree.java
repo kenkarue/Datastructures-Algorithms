2ad742c6a9c09159fb0148d428ec7f3e76ae6527
kenkarue <50879306+kenkarue@users.noreply.github.com>
2022-02-02 13:43:12 +0300

    Shared tree class

---
+++ src/main/java/SharedClasses/Tree.java
@@ -0,0 +1,14 @@
+package SharedClasses;
+
+public class Tree {
+
+    public Tree left;
+    public Tree right;
+    public int val;
+
+    public Tree(int val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
