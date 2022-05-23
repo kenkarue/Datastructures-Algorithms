6354b669693495e6dcce0c45af463f580fa28629
kenkarue <50879306+kenkarue@users.noreply.github.com>
2022-05-23 15:27:08 +0300

    Full Binary Tree - Parent has two or no children

---
+++ src/main/java/DSBinaryTrees/FullBinaryTree.java
@@ -0,0 +1,39 @@
+package DSBinaryTrees;
+
+import SharedClasses.Tree;
+
+/**
+ * Each node has Two or No children
+ */
+public class FullBinaryTree {
+
+    public static void main(String[] args){
+        Tree head = new Tree(20);
+        head.left = new Tree(10);
+        head.left.left = new Tree(5);
+        head.left.right = new Tree(15);
+
+        head.right = new Tree(30);
+        head.right.left = new Tree(25);
+        head.right.right = new Tree(35);
+        head.right.left.left = new Tree(23);
+        head.right.right.right = new Tree(40);
+
+        boolean isFullBinary = solution(head);
+        System.out.println("Is Full Binary :: "+isFullBinary);
+    }
+
+    private static boolean solution(Tree current){
+        if(current == null){
+            return true;
+        }
+        Tree left = current.left;
+        Tree right = current.right;
+        if((left == null && right != null) || (left != null && right == null)){
+            return false;
+        }
+        boolean isLeft = solution(left);
+        boolean isRight = solution(right);
+        return isLeft && isRight;
+    }
+}
